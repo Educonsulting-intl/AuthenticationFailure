f8b97b3c5014e2a9004bd6728cdcfe918c744522
matanrozen <rozenmatan1989@gmail.com>
2021-03-08 13:05:51 +0200

    add kiev and lviv vpns

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -26,6 +26,7 @@ public class BaseTest {
 	
 	protected WebDriver driver;
 	private DesiredCapabilities capabilities;
+	private String log;
 	
 	@Parameters({"log"})
 	@BeforeSuite
@@ -52,15 +53,26 @@ public class BaseTest {
 			driver.get("http://192.168.20.162:5601/app/discover#/?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-5m,to:now))&_a=(columns:!(_source),filters:!(),index:'5cdaac10-5425-11eb-935b-0bb49ce79efe',interval:auto,query:(language:kuery,query:'event.outcome%20:%20%22failure%22%20%20AND%20winlog.event_data.LogonProcessName:%20%22User32%20%22%20AND%20host.name%20:%20%22TS01%22%20'),sort:!())");
 		else if(log.equals("ManagementTS"))
 			driver.get("http://192.168.20.162:5601/app/discover#/?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-5m,to:now))&_a=(columns:!(_source),filters:!(),index:'5cdaac10-5425-11eb-935b-0bb49ce79efe',interval:auto,query:(language:kuery,query:'event.outcome%20:%20%22failure%22%20AND%20host.name%20:%20%22edu-ad01.extr.local%22%20%20'),sort:!())");
-		
+		else if(log.equals("LvivVpn"))
+			driver.get("http://192.168.20.162:5601/app/discover#/?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-1h,to:now))&_a=(columns:!(_source),filters:!(),index:'filebeat-*',interval:auto,query:(language:kuery,query:'observer.name%20:%22FG100FT19013741%22%20AND%20fortinet.firewall.status:%20%22failure%22%20AND%20fortinet.firewall.result:%20%22XAUTH%20authentication%20failed%22%20'),sort:!())");
+		else if(log.equals("KievVpn"))
+			driver.get("http://192.168.20.162:5601/app/discover#/?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-1h,to:now))&_a=(columns:!(_source),filters:!(),index:'filebeat-*',interval:auto,query:(language:kuery,query:'observer.name%20:%22FG100ETK18029390%22%20AND%20fortinet.firewall.status:%20%22failure%22%20AND%20fortinet.firewall.result:%20%22XAUTH%20authentication%20failed%22%20'),sort:!())");
+		this.log = log;
+
 	}
 	
 	@Test
 	public void getHits() {
 		MainPage mainPage = new MainPage(driver);
 		
+		if(log.equals("KievVpn") || log.equals("LvivVpn")) {
+			
+			AssertJUnit.assertTrue(Integer.parseInt(mainPage.getHits()) < 5);
+		}else {
+			
+			AssertJUnit.assertEquals("" , mainPage.getHits());
+		}
 
-		AssertJUnit.assertEquals("" , mainPage.getHits());
 	}
 	
 	@AfterSuite
